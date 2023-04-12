611f13aa20eb3a33163e5b4b57a94c6d65edc6a0
humi53 <j.k.humi9@gmail.com>
2023-04-12 13:59:07 +0900

    reFactory

---
+++ Hello.java
@@ -0,0 +1,8 @@
+//	This is JavaFile.
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("Hello World.");
+		System.out.println(20 + 30);	
+		System.out.println("me too.");
+	}
+}
